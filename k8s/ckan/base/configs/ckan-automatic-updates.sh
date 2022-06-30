3686b56697f7db013cd70cff67afa0568eca9cae
Enes Golic <XENEG@orsted.com>
2022-06-30 15:29:31 +0200

    CKAN changes

--- k8s/ckan/base/configs/ckan-automatic-updates.sh
+++ k8s/ckan/base/configs/ckan-automatic-updates.sh
@@ -7,4 +7,4 @@ export PYTHONWARNINGS="ignore:Unverified HTTPS request"
 
 queue_url="$AZURE_QUEUE_URL"
 
-python3 /usr/lib/ckan/orsted-scripts/scripts/project_import/main.py --api-key $API_KEY --url "$CKAN_SITE_URL"/api/3 --ssl-no-verify --azure-queue-name ckan --azure-queue-archive-path /usr/lib/ckan/msg-backup --azure-queue-connection-string "$AZURE_QUEUE_CONN"+"$CKAN_QUEUE_CONN_STRING"
+python3 /usr/lib/ckan/orsted-scripts/scripts/project_import/main.py --api-key $API_KEY --url "$CKAN_SITE_URL"/api/3 --ssl-no-verify --azure-queue-name ckan --azure-queue-archive-path /var/lib/ckan/msg-backup --azure-queue-connection-string "$AZURE_QUEUE_CONN"+"$CKAN_QUEUE_CONN_STRING"
